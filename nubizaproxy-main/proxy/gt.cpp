fff0d27ede5baed62ee10ee85d629d009f89d6b0
Almanyarra <93093548+Almanyarra@users.noreply.github.com>
2024-06-08 08:30:50 +0300

    Update gt.cpp

--- nubizaproxy-main/proxy/gt.cpp
+++ nubizaproxy-main/proxy/gt.cpp
@@ -4,7 +4,7 @@
 #include "utils.h"
 
 std::string gt::version = "4.58";
-std::string gt::flag = "id";
+std::string gt::flag = "kr";
 bool gt::resolving_uid2 = false;
 bool gt::connecting = false;
 bool gt::in_game = false;
